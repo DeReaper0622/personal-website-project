81e8cc27a48f16dcf980a942b0e4f7cc4768d5ee
DeReaper0622 <dereaper@hotmail.com>
2019-01-20 17:01:55 -0500

    check in the top image slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'George Yang';
+
+  imageSliderArray = [
+    { image: 'https://mdbootstrap.com/img/Photos/Others/images/77.jpg', alt: '', text: '' },
+    { image: 'https://mdbootstrap.com/img/Photos/Others/images/47.jpg', alt: '', text: '' },
+    { image: 'https://mdbootstrap.com/img/Photos/Others/images/79.jpg', alt: '', text: '' },
+  ];
 }
